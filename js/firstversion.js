7217dde15eef9309ac4f16d0e21164cd4d090116
kamarini <kama0549@stud.kea.dk>
2023-03-01 10:15:53 +0100

    lemonsters game

---
+++ js/firstversion.js
@@ -0,0 +1,367 @@
+let lives ;
+let points;
+let timeLeft;
+
+
+window.addEventListener("load", start);
+
+ 
+
+function start(){
+    console.log("start");
+    lives = 3;
+    points = 0;
+    timeLeft = 4;
+    document.querySelector("#life_board").classList.value="";
+    document.querySelector("#life_board").classList.add("live3");
+
+    document.querySelector("#score_board").classList.value="";
+    document.querySelector("#score_board").classList.add("point0");
+    showTitle();
+    
+     
+}
+
+function showTitle(){
+    console.log("showTitle");
+    document.querySelector("#title_screen").classList.remove("hidden");
+    document.querySelector("#play_button").addEventListener("click",startGame);
+    document.querySelector("#rules_button").addEventListener("click",showInstructions);
+  
+}
+
+function showInstructions(){
+    console.log('showInstructions');
+    hideAllScreens();
+    document.querySelector("#instructions").classList.remove("hidden");
+     document.querySelector("#play2_button").addEventListener("click",startGame);
+}
+
+function startGame() {
+    console.log(`startGame`);
+    hideAllScreens();
+    timeLeft = 4;
+    lives = 3;
+    points = 0;
+    document.querySelector("#life_board").classList.value="";
+    document.querySelector("#life_board").classList.add("live3");
+
+    document.querySelector("#score_board").classList.value="";
+    document.querySelector("#score_board").classList.add("point0");
+   
+   
+    document.querySelector("#title_screen").classList.add("hidden");
+    
+    document.querySelector("#lemon_container").classList.add("fade_out", "pos1");
+    document.querySelector("#lemon_container").addEventListener("click", LemonHit);
+    document.querySelector("#lemon_container").addEventListener("animationiteration", lemonAgain );
+    
+   document.querySelector("#watermellon_container").classList.add("pulse", "pos4");
+   document.querySelector("#watermellon_container").addEventListener("click", watermellonHit);
+   document.querySelector("#watermellon_container").addEventListener("animationiteration", watermellonAgain);
+
+   document.querySelector("#apple_container").classList.add("rotation2", "pos3");
+   document.querySelector("#apple_container").addEventListener("click", appleHit);
+   document.querySelector("#apple_container").addEventListener("animationiteration", appleAgain);
+
+    document.querySelector("#orange_container").classList.add("pattern", "pos12");
+    document.querySelector("#orange_container").addEventListener("click", orangeHit);
+    document.querySelector("#orange_container").addEventListener("animationiteration", orangeAgain);
+  
+    startTimer();
+    let birdsAudio = document.querySelector('#birds');
+      birdsAudio.play();
+
+   
+}
+
+function gainPoint(){
+    points = points + 1;
+  
+    console.log("gainPoint");
+
+
+} 
+function LemonHit(){
+    document.querySelector("#lemon_container").classList.add("stop");
+    document.querySelector("#lemon").classList.add("rotation");
+    document.querySelector("#lemon").addEventListener("animetionend", lemonAgain);
+    gainPoint();
+if(points===1){
+        document.querySelector("#score_board").classList.value="";
+        document.querySelector("#score_board").classList.add("point1");
+    }
+    if(points===2){
+        document.querySelector("#score_board").classList.value="";
+        document.querySelector("#score_board").classList.add("point2");
+    }
+
+    if(points===3){
+        document.querySelector("#score_board").classList.value="";
+        document.querySelector("#score_board").classList.add("point3");
+    }
+    if(points===4){
+        document.querySelector("#score_board").classList.value="";
+        document.querySelector("#score_board").classList.add("point4");
+    }
+    if(points===5){
+        document.querySelector("#score_board").classList.value="";
+        document.querySelector("#score_board").classList.add("point5");
+    }
+    if(points===6){
+        document.querySelector("#score_board").classList.value="";
+        document.querySelector("#score_board").classList.add("point6");
+    }
+    if(points===7){
+        document.querySelector("#score_board").classList.value="";
+        document.querySelector("#score_board").classList.add("point7");
+        endOfTheGame();
+
+    }
+ 
+}
+
+function lemonAgain() {
+    document.querySelector("#lemon_container").classList.value="";
+    document.querySelector("#lemon").classList.value="";
+    document.querySelector("#lemon").addEventListener("animationend", lemonAgain);
+
+    let randomPosition = generateRandomNumber(12);
+    document.querySelector("#lemon_container").classList.add("zoom_out", "pos" +randomPosition);
+   
+}
+function generateRandomNumber(num) {
+    return Math.floor(Math.random() * num) +1;
+}
+
+
+//------------ watermellon------------------
+
+
+function loseLife(){
+    console.log("loseLife");
+    lives = lives - 1;
+}
+
+function watermellonHit(){
+    document.querySelector("#watermellon_container").classList.add("stop");
+    document.querySelector("#watermellon").classList.add("pulse2");
+    document.querySelector("#watermellon").addEventListener("animetionend", watermellonAgain);
+    let ahhnejAudio = document.querySelector('#ahhnej');
+    ahhnejAudio.play();
+    loseLife();
+    if(lives===3){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live3");
+
+    }
+    if(lives===2){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live2");
+
+    }
+    if(lives===1){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live1");
+
+    }
+    if(lives===0){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live0");
+        endOfTheGame();
+
+    }
+}
+
+function watermellonAgain() {
+    document.querySelector("#watermellon_container").classList.value="";
+    document.querySelector("#watermellon").classList.value="";
+    document.querySelector("#watermellon").addEventListener("animationend", watermellonAgain);
+
+    let randomPosition = generateRandomNumber(8);
+    document.querySelector("#watermellon_container").classList.add("zoom_out", "pos" +randomPosition);
+   
+}
+
+
+
+
+//------------ apple ------------------
+
+function appleHit(){
+    document.querySelector("#apple_container").classList.add("stop");
+    document.querySelector("#apple").classList.add("pulse2");
+    document.querySelector("#apple").addEventListener("animetionend", appleAgain);
+    let ahhnejAudio = document.querySelector('#ahhnej');
+    ahhnejAudio.play();
+    loseLife();
+    if(lives===3){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live3");
+
+    }
+
+    if(lives===2){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live2");
+
+    }
+    if(lives===1){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live1");
+
+    }
+    if(lives===0){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live0");
+       endOfTheGame();
+
+    }
+}
+
+function appleAgain() {
+    document.querySelector("#apple_container").classList.value="";
+    document.querySelector("#apple").classList.value="";
+    document.querySelector("#apple").addEventListener("animationend", appleAgain);
+
+    let randomPosition = generateRandomNumber(8);
+    document.querySelector("#apple_container").classList.add("rotation2", "pos" +randomPosition);
+   
+}
+
+//------------ orange ------------------
+
+function orangeHit(){
+    document.querySelector("#orange_container").classList.add("stop");
+    document.querySelector("#orange").classList.add("pulse2");
+    document.querySelector("#orange").addEventListener("animetionend", orangeAgain);
+    let ahhnejAudio = document.querySelector('#ahhnej');
+    ahhnejAudio.play();
+    loseLife();
+    if(lives===3){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live3");
+
+    }
+
+    if(lives===2){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live2");
+
+    }
+    if(lives===1){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live1");
+
+    }
+    if(lives===0){
+        document.querySelector("#life_board").classList.value="";
+        document.querySelector("#life_board").classList.add("live0");
+        endOfTheGame();
+
+    }
+}
+
+function orangeAgain() {
+    document.querySelector("#orange_container").classList.value="";
+    document.querySelector("#orange").classList.value="";
+    document.querySelector("#orange").addEventListener("animationend", orangeAgain);
+
+    let randomPosition = generateRandomNumber(8);
+    document.querySelector("#orange_container").classList.add("pattern", "pos" +randomPosition);
+   
+}
+
+
+// --------timer------
+
+window.addEventListener("load", startTimer);
+
+
+
+
+function startTimer() {
+    console.log(`startTimer`);
+
+    document.querySelector("#time").classList.value = "timer" + timeLeft;
+    setTimeout(changeImage, 9000);
+}
+
+function changeImage() {
+    console.log("changeImage");
+    timeLeft--;
+
+  
+    if (timeLeft > 1) {
+        console.log("timeLeft: " + timeLeft);
+        document.querySelector("#time").classList.value = "timer" + timeLeft;
+        startTimer();
+    } else {
+        console.log("timeLeft: " + timeLeft);
+        document.querySelector("#time").classList.value = "timer" + timeLeft;
+        endOfTheGame();
+    }
+}
+
+// ----------other screens ------------
+
+function endOfTheGame()   {
+    console.log(`That's  all folks`);
+
+    document.querySelector("#lemon_container").classList.value="";
+    document.querySelector("#watermellon_container").classList.value="";
+    document.querySelector("#orange_container").classList.value="";
+    document.querySelector("#apple_container").classList.value="";
+
+    document.querySelector("#lemon_container").removeEventListener("click", LemonHit);
+    document.querySelector("#watermellon_container").removeEventListener("click", watermellonHit);
+    document.querySelector("#apple_container").removeEventListener("click", appleHit);
+    document.querySelector("#orange_container").removeEventListener("click", orangeHit);
+
+    document.querySelector("#lemon_container").removeEventListener("animationiteration", lemonAgain);
+    document.querySelector("#watermellon_container").removeEventListener("animationiteration", watermellonAgain);
+    document.querySelector("#apple_container").removeEventListener("animationiteration", appleAgain);
+    document.querySelector("#orange_container").removeEventListener("animationiteration", orangeAgain);
+   
+
+
+
+    if(points == 7 && lives > 0) {
+        youWon();
+    } else {
+        gameOver();
+    }
+}
+function gameOver() {
+    console.log("gameOver");
+    document.querySelector("#game_over").classList.remove("hidden");
+    document.querySelector("#play_again_game_over").addEventListener("click", startGame);
+    document.querySelector("#exit_game_over").addEventListener("click", start);
+    // ahhnejAudio.pause();
+    let nejAudio = document.querySelector('#nej');
+    nejAudio.play();
+
+    
+   
+}
+
+function youWon() {
+    console.log("youWon");
+    document.querySelector("#you_won").classList.remove("hidden");
+    document.querySelector("#play_again_you_won").addEventListener("click", startGame);
+    document.querySelector("#exit_you_won").addEventListener("click", start);
+   
+    let yeeeyAudio = document.querySelector('#yeeey');
+      yeeeyAudio.play();
+
+   
+}
+
+
+
+function hideAllScreens() {
+    document.querySelector("#title_screen").classList.add("hidden");
+    document.querySelector("#instructions").classList.add("hidden");
+    document.querySelector("#game_over").classList.add("hidden");
+    document.querySelector("#you_won").classList.add("hidden");
+}
